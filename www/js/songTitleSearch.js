032ff9d31aa6cd6e7e8906aa12ad331ebd5db769
DanTutt <dan_tuttle@msn.com>
2016-02-22 21:07:25 -0700

    Update the Music on click
    Made it so the music would start on-click, and stop on double-click.
    Probably not the way we want it, but at least it’s progress.
    I also made it stop the music on input change, and with the pause
    button. There’s quite a bit of tweeking, but I think I’m on the right
    track.

--- www/js/songTitleSearch.js
+++ www/js/songTitleSearch.js
@@ -11,15 +11,25 @@ function TitleController($http) {
     tc.song = '';
     tc.found = '';
     tc.songTitleSearch = songTitleSearch;
+    tc.audioObject = null;
+    tc.playTrack = playTrack;
+    tc.pauseTrack = pauseTrack;
 
     function songTitleSearch(song) {
         $http.get('https://api.spotify.com/v1/search?q=' + song + '&type=track').then(function (response) {
             tc.results = response.data.tracks.items[0].album.images[1].url;
+            tc.audioObject = new Audio(response.data.tracks.items[0].preview_url);
+
         });
         console.log(song);
         tc.found = '';
     }
-
+    function playTrack() {
+        tc.audioObject.play();
+    }
+    function pauseTrack() {
+        tc.audioObject.pause();
+    }
 }
 
 
